34fd54f5fbdd51571692661bda10fe6b540281e6
mannanisgreat <mannanisgreat@gmail.com>
2021-01-17 19:06:26 +0530

    first commit

---
+++ src/main/java/com/example/myrepo/MyrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myrepo/MyrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
